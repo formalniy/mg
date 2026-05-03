f4677027f3d3b1ad2107522f00392cda0d653e6f
formalniy <103324479+formalniy@users.noreply.github.com>
2026-05-03 15:37:56 +0300

    Delete moneyglitch directory

--- moneyglitch/target/debug/deps/moneyglitch-3a55757b2128da02.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Привет\Documents\rustmoneyglitch\moneyglitch\target\debug\deps\libmoneyglitch-3a55757b2128da02.rmeta: src/main.rs
-
-C:\Users\Привет\Documents\rustmoneyglitch\moneyglitch\target\debug\deps\moneyglitch-3a55757b2128da02.d: src/main.rs
-
-src/main.rs:
